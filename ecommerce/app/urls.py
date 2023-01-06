672c045e5801375c52db1c1c5469c252f7ec8297
networkdavit <networkdavit@gmail.com>
2023-01-06 23:04:29 +0400

    Changes day 2

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path("",views.home),
-]
+    path("category/<slug:val>",views.CategoryView.as_view(), name="category"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
