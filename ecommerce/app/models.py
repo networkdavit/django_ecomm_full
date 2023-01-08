5c923e1ac7cc5bd5a108ab9c25b53ac4b6870647
networkdavit <networkdavit@gmail.com>
2023-01-08 15:33:21 +0400

    Working on user, day 3

--- ecommerce/app/models.py
+++ ecommerce/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY_CHOICES=(
@@ -12,6 +13,17 @@ CATEGORY_CHOICES=(
     ("IC", "Ice-Creams"),
 )
 
+STATE_CHOICES=(
+    ("Yerevan", "Yerevan"),
+    ("Lori", "Lori"),
+    ("Shirak", "Shirak"),
+    ("Ararat", "Ararat"),
+    ("Syunik", "Syunik"),
+    ("Tavush", "Tavush"),
+    ("Kotayk", "Kotayk"),
+    ("Armavir", "Armavir"),
+)
+
 class Product(models.Model):
     title  = models.CharField(max_length=100)
     selling_price = models.FloatField()
@@ -23,3 +35,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def __str__(self):
         return self.title
+
+class Customer(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
